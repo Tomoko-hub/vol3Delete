83b063eeec0a74d63f6d3184084af35e4e5d1cab
Tomoko <haisaitomoco@gmail.com>
2021-09-28 15:43:33 +0300

    delete button ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      todos:[
+        {id: "1", text:"to do 1"},
+        {id: "2", text:"to do 2"},
+        {id: "3", text:"to do 3"},
+      ]
+    };
+  }
+
+  deleteTodo(id) {
+    let todos=this.state.todos;
+    todos = todos.filter((todo)=> todo.id !== id);
+    this.setState({todos:todos});
+  }
+
+  render() {
+    return(
+      <ul>
+        {
+          this.state.todos.map((todo) => {
+            return(
+              <li key={todo.id}>{todo.text}
+                <button onClick={this.deleteTodo.bind(this,todo.id)}>X</button>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
 }
 
 export default App;
